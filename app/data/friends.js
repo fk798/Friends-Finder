aecc91b8dc52b0b28a50216b21e9ce6136ac1866
fk798 <fk798@nyu.edu>
2018-09-15 23:41:10 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+    {
+        name: "Ninja",
+        photo: "https://i.ytimg.com/vi/nzCIXCMzbes/maxresdefault.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+]
+
+module.exports = friends
